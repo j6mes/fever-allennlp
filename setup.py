f883a990cb7d21c713a42ab451173ecd9d5faebc
James Thorne <james@jthorne.co.uk>
2019-01-18 10:46:25 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ install = [req for req in reqs if not req.startswith("git+git://")]
 depends = [req.replace("git+git://", "git+http://") for req in reqs if req.startswith("git+git://")]
 
 setup(
-    name='fever-allennlp-reader',
+    name='fever-allennlp',
     version='0.0.0',
     author='James Thorne',
     author_email='james@jamesthorne.co.uk',
     url='https://jamesthorne.co.uk',
-    description='Fact Extraction and VERification dataset reader for AllenNLP',
+    description='Fact Extraction and VERification experiments built on AllenNLP',
     long_description="readme",
     license=license,
     python_requires='>=3.6',
     package_dir={'fever': 'src/fever',
-                 'fever.reader': 'src/fever/reader'},
+                 'fever.submission': 'src/fever/submission'},
     packages=['fever',
-              'fever.reader'
+              'fever.submission'
               ],
     install_requires=install,
     dependency_links=depends,
